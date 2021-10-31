213c2cd89c0c3c25c76d726589a6ddd06ba110c2
Lundez <hampus.londogard@gmail.com>
2021-10-31 21:51:37 +0100

    Adding scores, confMatrix etc

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
+# %%
 import joblib
 import yaml
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
 from datasets import load_from_disk
 from joblib import dump
+import matplotlib.pyplot as plt
+import json
 
 # %%
 def get_params() -> str:
@@ -25,11 +28,13 @@ clf.fit([x[0] for x in train['image']], train['label'])
 y_pred = clf.predict([x[0] for x in test['image']])
 
 print(f"Accuracy: {accuracy_score(test['label'], y_pred)}")
+
+disp = ConfusionMatrixDisplay.from_predictions(test['label'], y_pred, normalize='true', cmap=plt.cm.Blues)
+
+with open('scores.json', 'w') as f:
+    json.dump({'avg_accuracy': accuracy_score(test['label'], y_pred), }, f)
+
+plt.savefig('confusion_matrix.png')
 joblib.dump(clf, 'model.joblib')
 
 # VGG16, LinearRegression, Vanilla CNN
-
-# CI/CD
-# data versioning
-# versioning
-# metric tracking
