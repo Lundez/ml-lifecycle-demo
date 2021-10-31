4ea00a34d9627002f2f9e2f1ad208747136ca3a6
Lundez <hampus.londogard@gmail.com>
2021-10-31 15:08:56 +0100

    Trained first model

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
+import joblib
 import yaml
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score
 from datasets import load_from_disk
+from joblib import dump
 
 # %%
 def get_params() -> str:
@@ -11,12 +14,18 @@ def get_params() -> str:
 dataset = load_from_disk("data/")
 params = get_params()
 
-print(params)
-print(dataset)
 
 # %%
+train = dataset['train'].to_pandas()
+test = dataset['test'].to_pandas()
 
+clf = LogisticRegression(random_state=42)
+clf.fit([x[0] for x in train['image']], train['label'])
 
+y_pred = clf.predict([x[0] for x in test['image']])
+
+print(f"Accuracy: {accuracy_score(test['label'], y_pred)}")
+joblib.dump(clf, 'model.joblib')
 
 # VGG16, LinearRegression, Vanilla CNN
 
