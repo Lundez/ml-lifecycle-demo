56c2aecd33171a2ea179444373e6e92166fdf7dd
Lundez <hampus.londogard@gmail.com>
2021-11-01 10:03:51 +0100

    Fix WF & code

--- train.py
+++ train.py
@@ -1,12 +1,17 @@
 # %%
 import joblib
 import yaml
+import json
+from joblib import dump
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
+
 from datasets import load_from_disk
-from joblib import dump
+
 import matplotlib.pyplot as plt
-import json
+import pandas as pd
+
 
 # %%
 def get_params() -> str:
@@ -22,17 +27,21 @@ params = get_params()
 train = dataset['train'].to_pandas()
 test = dataset['test'].to_pandas()
 
+train['flat_img'] = train['image']
+test['flat_img'] = test['image']
+
 clf = LogisticRegression(random_state=42)
-clf.fit([x[0] for x in train['image']], train['label'])
+clf.fit(train['flat_img'].to_numpy().reshape(60000, 28*28), train['label'])
 
-y_pred = clf.predict([x[0] for x in test['image']])
+y_pred = clf.predict(test['flat_img'].to_numpy().reshape(10000, 28*28))
 
-print(f"Accuracy: {accuracy_score(test['label'], y_pred)}")
-
+accuracy = accuracy_score(test['label'], y_pred)
 disp = ConfusionMatrixDisplay.from_predictions(test['label'], y_pred, normalize='true', cmap=plt.cm.Blues)
 
+print(f"Accuracy: {accuracy}")
+
 with open('scores.json', 'w') as f:
-    json.dump({'avg_accuracy': accuracy_score(test['label'], y_pred), }, f)
+    json.dump({'avg_accuracy': accuracy, }, f)
 
 plt.savefig('confusion_matrix.png')
 joblib.dump(clf, 'model.joblib')
