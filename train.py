89d0083ed40e01c9f3bd85d5eedde0bcbf97d6f3
Lundez <hampus.londogard@gmail.com>
2021-11-03 08:23:45 +0100

    vgg16

--- train.py
+++ train.py
@@ -7,15 +7,8 @@ from fastai import *
 from fastai.vision.all import *
 from dvclive.fastai import DvcLiveCallback
 
-<<<<<<< HEAD
-from fastai.vision.all import *
-from dvclive.fastai import DvcLiveCallback
-
-from datasets import load_from_disk
-=======
 from datasets import load_from_disk, concatenate_datasets
 from datasets.arrow_dataset import Dataset
->>>>>>> f23917f (Added DVCLive & fastai ResNet CNN)
 
 import matplotlib.pyplot as plt
 
@@ -24,11 +17,6 @@ def get_params() -> str:
     with open("params.yml") as f:
         return yaml.safe_load(f)
 
-<<<<<<< HEAD
-def reshape_data(example):
-    example['image'] = np.array(example['image']).reshape(28*28)
-    return example
-=======
 def get_data(source: Dataset):
     reshaped = torch.stack((source['image'],) * 3, axis=1).type(torch.FloatTensor)
     reshaped /= 255.
@@ -44,7 +32,6 @@ def get_data(source: Dataset):
     return res
 
 # %%
->>>>>>> f23917f (Added DVCLive & fastai ResNet CNN)
 
 # %%
 dataset = load_from_disk("data/")
@@ -55,19 +42,11 @@ params = get_params()
 
 
 # %%
-<<<<<<< HEAD
-train = dataset['train'].map(reshape_data)
-test = dataset['test'].map(reshape_data)
-
-clf = LogisticRegression(random_state=42)
-clf.fit(train['image'], train['label'])
-
-y_pred = clf.predict(test['image'])
-=======
 datablock = DataBlock(
     blocks=(ImageBlock(cls=PILImage), CategoryBlock),
     get_items=get_data,
     splitter=EndSplitter(len(dataset['test']) / len(all_data)),
+    item_tfms=Resize(64, ResizeMethod.Pad, pad_mode='zeros'),
     get_y=(lambda item: item['y']),
     get_x=(lambda item: item['x'])
 )
@@ -77,11 +56,10 @@ dls.show_batch()
 
 # %%
 
-learn = cnn_learner(dls, arch = models.resnet18, metrics=accuracy)
+learn = cnn_learner(dls, arch = models.vgg11_bn, metrics=accuracy)
 learn.fine_tune(1, cbs=[DvcLiveCallback()])
 
 interpret = ClassificationInterpretation.from_learner(learn)
->>>>>>> f23917f (Added DVCLive & fastai ResNet CNN)
 
 accuracy = learn.final_record[1]
 interpret.plot_confusion_matrix()
@@ -92,9 +70,5 @@ with open('scores.json', 'w') as f:
     json.dump({'avg_accuracy': accuracy, }, f)
 
 plt.savefig('confusion_matrix.png')
-<<<<<<< HEAD
-joblib.dump(clf, 'model.joblib')
-=======
 
 learn.save(os.path.abspath('./model'))
->>>>>>> f23917f (Added DVCLive & fastai ResNet CNN)
