e644f0b7b44874e745c64398c8b20242008db6aa
Lundez <hampus.londogard@gmail.com>
2021-11-17 14:04:16 +0100

    Removing and changing things for workshop

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ml-lifecycle-demo
 Demo for my presentation at Google DevFest2021 - "Managing the ML Lifecycle without a Headache"
+
+## Workshop Edition
+This is the workshop branch which allows us to implement it ourself.
--- train.py
+++ train.py
@@ -11,13 +11,15 @@ from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
 
 from datasets import load_from_disk
 # %%
+class TODO(Exception):
+     pass
+# %%
 def get_params() -> str:
     with open("params.yml") as f:
-        return yaml.safe_load(f)
+        raise TODO("Use yaml to load file")
 
 def reshape_data(example):
-    example['image'] = np.array(example['image']).reshape(28*28)
-    return example
+    raise TODO("example['image'] should be 28*28")
 
 # %%
 dataset = load_from_disk("data/")
@@ -25,21 +27,57 @@ params = get_params()
 
 
 # %%
-train = dataset['train'].map(reshape_data)
-test = dataset['test'].map(reshape_data)
+raise TODO("define train & test, then map the data into correct fmt")
 
-clf = LogisticRegression(random_state=42)
-clf.fit(train['image'], train['label'])
+raise TODO("Create a LogisticRegression and fit on data")
+require(type(clf) == LogisticRegression)
 
-y_pred = clf.predict(test['image'])
-
-accuracy = accuracy_score(test['label'], y_pred)
-disp = ConfusionMatrixDisplay.from_predictions(test['label'], y_pred, normalize='true', cmap=plt.cm.Blues)
+raise TODO("Predict with model and get accuracy + confusion matrix")
 
 print(f"Accuracy: {accuracy}")
 
 with open('scores.json', 'w') as f:
-    json.dump({'avg_accuracy': accuracy, }, f)
+    raise TODO("Dump score as avg_accuracy in json")
+
+raise TODO("Dump clf & confusionmatrix")
+
+# %%
+
+"""
+# FastAI + ResNet
+def get_data(source: Dataset):
+    reshaped = torch.stack((source['image'],) * 3, axis=1).type(torch.FloatTensor)
+    reshaped /= 255.
+
+    result = [PILImage(to_image(x)) for x in reshaped]
+    labels = source['label']
+
+    res = []
+    for i in range(0, len(result)):
+        data = {'x': result[i], 'y': str(labels[i].item())}
+        res.append(data)
+
+    return res
+
+datablock = DataBlock(
+    blocks=(ImageBlock(cls=PILImage), CategoryBlock),
+    get_items=get_data,
+    splitter=EndSplitter(len(dataset['test']) / len(all_data)),
+    item_tfms=Resize(64, ResizeMethod.Pad, pad_mode='zeros'),
+    get_y=(lambda item: item['y']),
+    get_x=(lambda item: item['x'])
+)
+dls = datablock.dataloaders(all_data)
+
+dls.show_batch()
+
+learn = cnn_learner(dls, arch = models.resnet18, metrics=accuracy)
+learn.fine_tune(1, cbs=[DvcLiveCallback()])
+
+accuracy = learn.final_record[1]
+interpret.plot_confusion_matrix()
 
 plt.savefig('confusion_matrix.png')
-joblib.dump(clf, 'model.joblib')
+learn.save(os.path.abspath('./model'))
+
+"""
